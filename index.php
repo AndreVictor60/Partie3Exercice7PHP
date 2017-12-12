74be5e5182b645fd641f1630e376c2fdce8fa809
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:11:07 +0100

    fini

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice 7</title>
+  </head>
+  <body>
+    <?php
+      for($pas=15;$pas <= 100;$pas+=15){
+        echo 'On tient le bon bout.<br />';
+      }
+     ?>
+  </body>
+</html>
